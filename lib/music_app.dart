660f7baed083f59e342dd6c485fb8ed74228b035
Were Samson Bruno <werefast2000@gmail.com>
2024-01-18 11:04:56 +0000

    renamed "musc" to "music"

--- lib/music_app.dart
+++ lib/music_app.dart
@@ -14,7 +14,7 @@ class MusicApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MuscListPage(),
+      home: const MusicListPage(),
     );
   }
 }
