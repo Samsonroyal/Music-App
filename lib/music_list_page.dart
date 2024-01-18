9a72af4e5265973dce9b47a84f8c60b1a63d20d3
Were Samson Bruno <werefast2000@gmail.com>
2024-01-18 10:19:50 +0000

    Added bottom Navbar and instantiated active index to enable onTap actions

--- lib/music_list_page.dart
+++ lib/music_list_page.dart
@@ -10,7 +10,7 @@ class MuscListPage extends StatefulWidget {
 class _MuscListPageState extends State<MuscListPage> {
   List<Map<String, dynamic>> feedItems = [
     {
-      "name": "Sweather Waether",
+      "name": "Sweater Weather",
       "title": "The Neighborhood",
       "profile":
           "https://www.nairaland.com/attachments/12420653_img20200922123338_jpegc3324609d3a50ff0aa04abe7a3a7376e",
@@ -81,10 +81,36 @@ class _MuscListPageState extends State<MuscListPage> {
     }
   ];
 
+  int activeIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card), label: "Payments"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.arrow_upward), label: "Send"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: "Notifications"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Settings"),
+        ],
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blueAccent,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: (value) {
+          print("Selected Index: " + value.toString());
+          setState(() {
+            activeIndex = value;
+          });
+        },
+        currentIndex: activeIndex,
+      ),
       appBar: AppBar(
         elevation: 4,
         backgroundColor: Colors.black,
