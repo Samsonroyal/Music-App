9ec51049f59339f66217a7ec3ea638c2ce03b0d8
Were Samson Bruno <werefast2000@gmail.com>
2024-01-18 11:12:26 +0000

    fixed errors and conflicts in music home page

--- lib/music_home_page.dart
+++ lib/music_home_page.dart
@@ -1,41 +1,43 @@
 import 'package:flutter/material.dart';
 
 class MusicHomePage extends StatefulWidget {
-  int currentIndex;
-  final Function(int) onTap;
+  const MusicHomePage({super.key});
 
-  const MusicHomePage({
-    super.key,
-    required this.currentIndex,
-    required this.onTap,
-  });
+  @override
+  State<MusicHomePage> createState() => _MusicHomePageState();
+}
+
+class _MusicHomePageState extends State<MusicHomePage> {
+  int currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.credit_card), label: "Payments"),
-            BottomNavigationBarItem(icon: Icon(Icons.arrow_upward), label: "Send",),
-            BottomNavigationBarItem(icon: Icon(Icons.notifications), label: "Notifications",),
-            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Settings" ),
-      ],
-      type: BottomNavigationBarType.fixed,
-      selectedItemColor: Colors.blueAccent,
-      showSelectedLabels: true,
-      showUnselectedLabels: true,
-      onTap: (index){
-        setState((){
-          activeIndex = index
-        });
-      },
-      currentIndex: activeIndex,
-        ),
-    body: Container(
-
-    ),
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.credit_card), label: "Payments"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.arrow_upward), label: "Send"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.notifications), label: "Notifications"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings), label: "Settings"),
+        ],
+        type: BottomNavigationBarType.fixed,
+        selectedItemColor: Colors.blueAccent,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index; // Update currentIndex within the state
+          });
+        },
+        currentIndex: currentIndex, // Use the currentIndex from the state
+      ),
+      body: Container(), // Replace with your actual body content
     );
   }
 }
